a74cafe19f2cf5c264f12e9ae0debc6a4ff64553
Jove <jovezhong@gmail.com>
2012-11-25 15:36:53 +0800

    upgrade a bunch of other grails plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -38,10 +38,12 @@ grails.project.dependency.resolution = {
 
     plugins {
         runtime ":hibernate:$grailsVersion"
-        runtime ":jquery:1.7.1"
+        runtime ":jquery:1.8.3"
         runtime ":resources:1.1.5"
 
         test ":spock:0.7"
+
+        //compile ':cloud-foundry:1.2.3'
 
         build ":tomcat:$grailsVersion"
     }
