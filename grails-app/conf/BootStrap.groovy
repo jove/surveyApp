365b5c3c9c67e0572741667e023ce838f8df6930
Jove <jovezhong@gmail.com>
2012-11-30 13:33:32 +0800

    Move the admin key to surveyApp-config.propeties, so that other folks can change it

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,13 +4,14 @@ import exp.Settings
 class BootStrap {
 
     def fixtureLoader
+    def grailsApplication
 
     def init = { servletContext ->
         if(Environment.current == Environment.DEVELOPMENT && System.properties.testData){
             fixtureLoader.load(System.properties.testData)
         }
         if(Settings.count()==0){
-    		new Settings(propKey:'adminKey',propValue:'210787').save(failOnError: true)
+    		new Settings(propKey:'adminKey',propValue: grailsApplication.config.survey_admin_key).save(failOnError: true)
     	}
     }
     def destroy = {
